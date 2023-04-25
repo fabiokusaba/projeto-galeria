365e8591c703a1308ec76853768d96ce89c480d5
Fabio Kusaba <fabinhokb44@gmail.com>
2023-04-25 11:40:15 -0300

    projeto galeria

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// SASS Files 
+import './scss/index.scss';
+
+// Dependencies
+import 'jquery';
+import 'bootstrap';
+
+// JS Files
+import './js/core/includes';
+import './js/plugins/cityButtons';
